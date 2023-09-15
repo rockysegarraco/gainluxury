3c7bda232bfa5e3219854bc433106f59f0e045c8
Rocky Segarra <rockysegarra@icloud.com>
2023-09-15 12:52:08 -0400

    component issue

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from "./components/header.js";
+import fullWidthMenu from "./components/erase.js";
 
 import logo from "./logo.svg";
 import "./App.css";
@@ -7,21 +8,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p class="uppercase ...">The quick brown fox ...</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <erase />
     </div>
   );
 }
