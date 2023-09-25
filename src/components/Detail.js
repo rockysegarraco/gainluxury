1bf3a778d64eb947f43e1cc3518bfe9752013819
Rocky Segarra <rockysegarra@icloud.com>
2023-09-25 12:24:58 -0400

    updated ui

---
+++ src/components/Detail.js
@@ -0,0 +1,147 @@
+/*
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    theme: {
+      extend: {
+        gridTemplateRows: {
+          '[auto,auto,1fr]': 'auto auto 1fr',
+        },
+      },
+    },
+    plugins: [
+      // ...
+      require('@tailwindcss/aspect-ratio'),
+    ],
+  }
+  ```
+*/
+import { useState } from "react";
+import { StarIcon } from "@heroicons/react/20/solid";
+import { RadioGroup } from "@headlessui/react";
+
+const product = {
+  name: "Basic Tee 6-Pack",
+  price: "$192",
+  href: "#",
+  breadcrumbs: [
+    { id: 1, name: "Men", href: "#" },
+    { id: 2, name: "Clothing", href: "#" },
+  ],
+  images: [
+    {
+      src: "https://tailwindui.com/img/ecommerce-images/product-page-02-secondary-product-shot.jpg",
+      alt: "Two each of gray, white, and black shirts laying flat.",
+    },
+    {
+      src: "https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-01.jpg",
+      alt: "Model wearing plain black basic tee.",
+    },
+    {
+      src: "https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-02.jpg",
+      alt: "Model wearing plain gray basic tee.",
+    },
+    {
+      src: "https://tailwindui.com/img/ecommerce-images/product-page-02-featured-product-shot.jpg",
+      alt: "Model wearing plain white basic tee.",
+    },
+  ],
+};
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function Example(props) {
+  return (
+    <>
+      <div className="bg-white">
+        <div className="pt-6">
+          <nav aria-label="Breadcrumb">
+            <ol
+              role="list"
+              className="mx-auto flex max-w-[2xl] items-center space-x-2 px-4 sm:px-6 lg:max-w-[90%] lg:px-8"
+            >
+              {product.breadcrumbs.map((breadcrumb) => (
+                <li key={breadcrumb.id}>
+                  <div className="flex items-center">
+                    <a
+                      href={breadcrumb.href}
+                      className="mr-2 text-sm font-medium text-gray-900"
+                    >
+                      {breadcrumb.name}
+                    </a>
+                    <svg
+                      width={16}
+                      height={20}
+                      viewBox="0 0 16 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                      className="h-5 w-4 text-gray-300"
+                    >
+                      <path d="M5.697 4.34L8.98 16.532h1.327L7.025 4.341H5.697z" />
+                    </svg>
+                  </div>
+                </li>
+              ))}
+              <li className="text-sm">
+                <a
+                  href={product.href}
+                  aria-current="page"
+                  className="font-medium text-gray-500 hover:text-gray-600"
+                >
+                  {product.name}
+                </a>
+              </li>
+            </ol>
+          </nav>
+
+          {/* Image gallery */}
+          <div className="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-[90%] lg:grid-cols-4 lg:gap-x-8 lg:px-8 bg-slate-100">
+            IMAGE GALLERY GOES HERE
+          </div>
+
+          {/* Product info */}
+          <div className="mx-auto max-w-2xl px-4 pb-16 pt-10 sm:px-6 lg:grid lg:max-w-[90%] lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pb-24 lg:pt-16">
+            <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
+              <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
+                {props.title}
+              </h1>
+            </div>
+
+            {/* Options */}
+            <div className="mt-4 lg:row-span-3 lg:mt-0">
+              <h2 className="sr-only">Product information</h2>
+              <p className="text-3xl tracking-tight text-gray-900">
+                {product.price}
+              </p>
+
+              {/* EMAIL */}
+              <div className="mt-10">
+                <button
+                  type="submit"
+                  className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                >
+                  Send Email
+                </button>
+              </div>
+            </div>
+
+            <div className="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pb-16 lg:pr-8 lg:pt-6">
+              {/* Description and details */}
+              <div>
+                <h3 className="sr-only">Description</h3>
+                <div className="space-y-6">
+                  <p className="text-base text-gray-900">{props.description}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/routes/PostDetail.jsx
+++ src/routes/PostDetail.jsx
@@ -1,48 +1,59 @@
-import React, { useState, useEffect } from 'react'
-import { collection, getDocs, query, where } from 'firebase/firestore';
-import CircularProgress from '@mui/material/CircularProgress';
-import Box from '@mui/material/Box';
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+import { useParams } from "react-router-dom";
+import Detail from "../components/Detail";
 //
 import db from "../firebase";
 
-
 const PostDetail = () => {
-    let { slug } = useParams();
-      const [isLoading, setIsLoading] = useState(true);
+  let { slug } = useParams();
+  const [isLoading, setIsLoading] = useState(true);
   const [data, setData] = useState();
 
   useEffect(() => {
     if (slug) {
-        getPost()
+      getPost();
     }
   }, [slug]);
 
-  const getPost = async() => {
-    setIsLoading(true)
-    const collections =  collection(db, 'cars');
-        const q = query(collections, where("slug", "==", slug));
-         const querySnapshot = await getDocs(q);
-         querySnapshot.forEach((doc) => {
-           // doc.data() is never undefined for query doc snapshots
-           setData(doc.data())
-         });
-         setIsLoading(false)
-  }
+  const getPost = async () => {
+    setIsLoading(true);
+    const collections = collection(db, "cars");
+    const q = query(collections, where("slug", "==", slug));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      setData(doc.data());
+    });
+    setIsLoading(false);
+  };
 
   return (
-    <div className='flex'>
-        {isLoading ? (<Box sx={{ flex: 1, height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-            <CircularProgress />  
-        </Box>) : (
-           <div>
-             <div>{data.title}</div>
-            <div>{data.description}</div>
-           </div>
-        )}
-      
+    <div>
+      <Detail title={data.title} description={data.description} />
+      {isLoading ? (
+        <Box
+          sx={{
+            flex: 1,
+            height: "100vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <CircularProgress />
+        </Box>
+      ) : (
+        <div>
+          <div>{data.title}</div>
+          <div>{data.description}</div>
+          <div>{data.price}</div>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default PostDetail
+export default PostDetail;
