7f87280e212fae4b1e135bb57040683d3cd9d6eb
Rocky Segarra <rockysegarra@icloud.com>
2023-09-25 08:16:25 -0400

    updated ui

--- src/components/pricing.js
+++ src/components/pricing.js
@@ -1,12 +1,3 @@
-import { CheckIcon } from "@heroicons/react/20/solid";
-
-const includedFeatures = [
-  "Private forum access",
-  "Member resources",
-  "Entry to annual conference",
-  "Official member t-shirt",
-];
-
 export default function Pricing() {
   return (
     <div className="bg-white">
--- src/routes/success.jsx
+++ src/routes/success.jsx
@@ -1,33 +1,57 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 import db from "../firebase";
-import { collection, addDoc } from 'firebase/firestore';
-const Success = () => {
+import { collection, addDoc } from "firebase/firestore";
+import Topbar from "../components/common/Topbar";
 
+const Success = () => {
   useEffect(() => {
     uploadData();
-  }, [])
+  }, []);
 
   const uploadData = () => {
-    const post = JSON.parse(localStorage.getItem('userPost'));
+    const post = JSON.parse(localStorage.getItem("userPost"));
     if (post) {
-     const collections =  collection(db, post.category.value);
+      const collections = collection(db, post.category.value);
       // Add a document to the collection
       addDoc(collections, {
-        ...post
+        ...post,
       })
         .then((docRef) => {
-          console.log('Document written with ID: ', docRef.id);
-          localStorage.removeItem('userPost')
+          console.log("Document written with ID: ", docRef.id);
+          localStorage.removeItem("userPost");
         })
         .catch((error) => {
-          console.error('Error adding document: ', error);
+          console.error("Error adding document: ", error);
         });
     }
-  }
+  };
 
   return (
-    <h3>Payment Success, You Ad posted Successfully, You can see your post in your profile or public home page.</h3>
-  )
-}
+    <>
+      <Topbar />
+      <div className="bg-white mt-24 lg:mt-36">
+        <div className="mx-auto max-w-2xl px-6 lg:px-8">
+          <div className="mx-auto mt-8 lg:mt-16 max-w-2xl rounded-3xl ring-1 ring-gray-200 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none">
+            <div className="p-6 lg:p-8 sm:p-10 lg:flex-auto">
+              <h3 className="text-3xl font-bold tracking-tight text-gray-900">
+                Success!
+              </h3>
+              <p className="mt-6 text-base leading-7 text-gray-600">
+                Lorem ipsum dolor sit amet consect etur adipisicing elit. Itaque
+                amet indis perferendis blanditi
+              </p>
+              <button
+                type="button"
+                className="rounded-full bg-black px-8 py-4 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 mt-8"
+              >
+                View Listing
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Success
+export default Success;
