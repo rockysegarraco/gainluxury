de0303a71e13b7f8a3525751ec7d0566a00fbd07
Rocky Segarra <rockysegarra@icloud.com>
2023-09-29 15:53:08 -0400

    Update login.jsx

--- src/routes/login.jsx
+++ src/routes/login.jsx
@@ -1,21 +1,24 @@
-import { SignIn } from '@clerk/clerk-react';
-import React from 'react'
-import { useTitle } from '../utils';
+import { SignIn } from "@clerk/clerk-react";
+import React from "react";
+import { useTitle } from "../utils";
+import Headings from "../components/Headings";
 
 const Login = () => {
-
-  useTitle("Login")
+  useTitle("Login");
 
   return (
-    <div className='flex h-screen'>
-        <div className='flex flex-1 items-center justify-center'>
-            <SignIn path='/login' signUpUrl='/signup' routing='path' />
+    <>
+      <Headings />
+      <div className="flex h-screen">
+        <div className="flex flex-1 items-center justify-center">
+          <SignIn path="/login" signUpUrl="/signup" routing="path" />
         </div>
-        <div className='flex flex-1 items-center justify-center'>
-            Add your design here
+        <div className="flex flex-1 items-center justify-center bg-slate-500">
+          Add your design here
         </div>
-    </div>
-  )
-}
+      </div>
+    </>
+  );
+};
 
 export default Login;
