70df2a74303f4e3a918653eb3f916659c1d163a1
SagarChavada <sgchavada2013@gmail.com>
2023-10-10 11:40:50 +0530

    implement Fade

---
+++ src/components/Carousel/CarouselFade.js
@@ -0,0 +1,124 @@
+import React, { useState, useEffect } from "react";
+import { IconButton } from "@material-tailwind/react";
+import "./Carousel.css";
+
+const Carousel = ({ children }) => {
+  const [counter, setCounter] = useState(1);
+  const [pause, setPause] = useState(false);
+  const content = children;
+
+  const handleNext = () => {
+    if (counter !== content.length) {
+      setCounter(counter + 1);
+    } else {
+      setCounter(1);
+    }
+  };
+
+  const handlePre = () => {
+    if (counter !== 1) {
+      setCounter(counter - 1);
+    } else {
+      setCounter(content.length);
+    }
+  };
+
+  const handlePage = page => {
+    setCounter(page);
+  };
+
+  const handleMouse = () => {
+    setPause(!pause);
+  };
+
+  useEffect(() => {
+    let interval = setInterval(() => {
+      if (!pause) {
+        handleNext();
+      } else {
+        clearInterval(interval);
+      }
+    }, 3000);
+    return () => clearInterval(interval);
+  });
+
+  return (
+    <div className="App">
+      <div
+        className="slide"
+        onMouseEnter={handleMouse}
+        onMouseLeave={handleMouse}
+      >
+        {content.map((item, index) => (
+          <div
+            className={counter - 1 === index ? "show" : "not-show"}
+            key={index}
+          >
+            {item}
+          </div>
+        ))}
+
+        <IconButton
+          variant="text"
+          color="white"
+          size="lg"
+          onClick={handlePre}
+          className="!absolute top-2/4 left-4 -translate-y-2/4"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={2}
+            stroke="currentColor"
+            className="h-6 w-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+            />
+          </svg>
+        </IconButton>
+
+        <IconButton
+          variant="text"
+          color="white"
+          size="lg"
+          onClick={handleNext}
+          className="!absolute top-2/4 !right-4 -translate-y-2/4"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={2}
+            stroke="currentColor"
+            className="h-6 w-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+            />
+          </svg>
+        </IconButton>
+      </div>
+
+
+      <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
+          {new Array(content.length).fill("").map((_, i) => (
+            <span
+              key={i}
+              className={`block h-1 cursor-pointer rounded-2xl transition-all content-[''] ${
+                counter - 1 === i ? "w-8 bg-white" : "w-4 bg-white/50"
+              }`}
+              onClick={() => handlePage(i + 1)}
+            />
+          ))}
+        </div>
+    </div>
+  );
+};
+
+export default Carousel;
