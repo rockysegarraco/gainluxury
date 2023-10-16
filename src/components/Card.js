cb62539d72adb6c5475bc049075f47b9e54a2bee
Rocky Segarra <rockysegarra@icloud.com>
2023-10-16 10:57:14 -0400

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,30 +1,30 @@
+import { Link } from "react-router-dom";
+
 export default function Example(props) {
   return (
     <>
       <article className="border">
         <div className="relative w-full">
-          <a href={props.link}>
+          <Link to={props.Link}>
             <img
               src={props.ImageUrl}
               alt={props.Alt}
               className="aspect-[16/9] w-full bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
             />
-          </a>
+          </Link>
         </div>
         <div className="mx-auto max-w-full p-3">
           <div className="group relative">
             <div className="flex justify-between">
-              <div>
+              <Link to={props.Link}>
                 <div className="font-bold text-base text-black/100">
                   {props.Price}
                 </div>
                 <div className="text-sm text-black/75">
-                  <a href={props.Link}>
-                    {props.Title}
-                    {/* <span aria-hidden="true" className="absolute inset-0" /> REmove this line */}
-                  </a>
+                  {props.Title}
+                  {/* <span aria-hidden="true" className="absolute inset-0" /> REmove this line */}
                 </div>
-              </div>
+              </Link>
               <div>
                 <button
                   className="rounded bg-black px-2 py-1 text-xs font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
