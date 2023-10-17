00c4f1c1b31341c6229a0c553236f0a5202aadab
Rocky Segarra <rockysegarra@icloud.com>
2023-10-17 12:42:13 -0400

    updated

--- src/routes/AddProperty.jsx
+++ src/routes/AddProperty.jsx
@@ -16,11 +16,19 @@ import Form from "../components/form";
 import TextInput from "../components/form/TextInput";
 import Select from "../components/form/Select";
 import Pricing from "../components/pricing";
-import { CATEGORY, COUNTRY, US_STATE } from "../utils/constants";
+import {
+  CATEGORY,
+  COUNTRY,
+  US_STATE,
+  RE_LISTING_TYPE,
+  RE_SELECT_PROPERTY,
+  RE_SELECT_PROPERTY_CATEGORY,
+} from "../utils/constants";
 import { uploadImages } from "../firebase";
 import { createSlug, deepCloneData, validatePhone } from "../utils";
 import { useEffect } from "react";
 import PhoneInput from "../components/form/PhoneInput";
+import { PhotoIcon, UserCircleIcon } from "@heroicons/react/24/solid";
 
 const { FormItem } = Form;
 
@@ -157,181 +165,259 @@ const AddProperty = ({ form }) => {
       <div className="mx-auto max-w-full lg:max-w-4xl bg-white">
         <Pricing />
       </div>
-      <div className="mx-auto max-w-full md:max-w-4xl lg:max-w-3xl mt-2 bg-white px-10 lg:px-0 py-5 mb-5">
-        <Stack spacing={0}>
-          <h2 className="text-2xl font-bold mb-4">Let's get started</h2>
-          <Stack gap={3}>
-            <div className="mt-4">
-              <FormItem>
-                {getFieldDecorator("title", {
-                  initialValue: "",
-                  rules: [{ required: true }],
-                })(<TextInput label="Listing Title" />)}
-              </FormItem>
-              <div className="mt-4">
+
+      <div className="mx-auto max-w-7xl grid grid-cols-12 gap-4">
+        <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow col-span-6">
+          <div className="px-4 py-5 sm:px-6 font-bold">
+            Listing Details
+            {/* We use less vertical padding on card headers on desktop than on body sections */}
+          </div>
+          <div className="px-4 py-5 sm:p-6">
+            <Stack spacing={0}>
+              <Stack gap={3}>
                 <FormItem>
-                  {getFieldDecorator("agent", {
+                  {getFieldDecorator("title", {
                     initialValue: "",
                     rules: [{ required: true }],
-                  })(<TextInput label="Agent" />)}
+                  })(<TextInput label="Listing Title" />)}
+                </FormItem>
+                <FormItem>
+                  {getFieldDecorator("listingType", {
+                    initialValue: "",
+                    rules: [{ required: true }],
+                  })(
+                    <Select
+                      options={RE_LISTING_TYPE}
+                      fullWidth
+                      label="Listing Type *"
+                    />
+                  )}
+                </FormItem>
+                <FormItem>
+                  {getFieldDecorator("propertyCategory", {
+                    initialValue: "",
+                    rules: [{ required: true }],
+                  })(
+                    <Select
+                      options={RE_SELECT_PROPERTY_CATEGORY}
+                      fullWidth
+                      label="Category *"
+                      autocomplete="off"
+                    />
+                  )}
+                </FormItem>
+                <div>
+                  <FormItem>
+                    {getFieldDecorator("price", {
+                      initialValue: "",
+                      rules: [{ required: false }],
+                    })(<TextInput label="Price [$] *" />)}
+                  </FormItem>
+                </div>
+                <FormItem>
+                  {getFieldDecorator("description", {
+                    initialValue: "",
+                    rules: [{ required: true }],
+                  })(<TextInput multiline label="Features" />)}
+                </FormItem>
+
+                <div class="col-span-full">
+                  <label
+                    for="cover-photo"
+                    class="block font-medium leading-6 text-gray-700"
+                  >
+                    Photos
+                  </label>
+                  <div class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                    <div class="text-center">
+                      <svg
+                        class="mx-auto h-12 w-12 text-gray-300"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z"
+                          clip-rule="evenodd"
+                        />
+                      </svg>
+                      <div class="mt-4 flex text-sm leading-6 text-gray-600">
+                        <label
+                          for="file-upload"
+                          class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                        >
+                          <span className="self-center">
+                            {galleryLoading ? "Loading..." : "Add Images"}
+                          </span>
+                          <input
+                            id="file-upload"
+                            name="file-upload"
+                            multiple
+                            max={5}
+                            type="file"
+                            class="sr-only"
+                            ref={inputGallery}
+                            accept="image/png, image/jpg, image/jpeg"
+                            onChange={handleGalleryFile}
+                          />
+                        </label>
+                      </div>
+                      <p class="text-xs leading-5 text-gray-600">
+                        PNG, JPG up to 3MB
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex overflow-x-auto gap-2 flex-row whitespace-nowrap">
+                  {gallaryImages.map((image, index) => (
+                    <Paper
+                      key={index}
+                      sx={{
+                        backgroundImage: `url(${image})`,
+                        backgroundRepeat: "no-repeat",
+                        backgroundSize: "cover",
+                        height: 150,
+                        width: 150,
+                        flexShrink: 0,
+                        marginTop: "10px",
+                      }}
+                    >
+                      <IconButton
+                        sx={{
+                          m: 1,
+                          height: "22px",
+                          width: "22px",
+                          bgcolor: "white",
+                        }}
+                        onClick={() => handleImageDelete(index)}
+                      >
+                        <CloseOutlined />
+                      </IconButton>
+                    </Paper>
+                  ))}
+                </div>
+              </Stack>
+            </Stack>
+          </div>
+        </div>
+        <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow col-span-6">
+          <div className="px-4 py-5 sm:px-6 font-bold">
+            Contact Details
+            {/* We use less vertical padding on card headers on desktop than on body sections */}
+          </div>
+          <div className="px-4 py-5 sm:p-6">
+            <Stack spacing={0}>
+              <div>
+                <FormItem>
+                  {getFieldDecorator("agentName", {
+                    initialValue: "",
+                    rules: [{ required: false }],
+                  })(<TextInput label="Agent Name" />)}
                 </FormItem>
               </div>
-            </div>
-            <FormItem>
-              {getFieldDecorator("description", {
-                initialValue: "",
-                rules: [{ required: true }],
-              })(<TextInput multiline label="Features" />)}
-            </FormItem>
-
-            <div class="col-span-full">
-              <label
-                for="cover-photo"
-                class="block font-medium leading-6 text-gray-700"
-              >
-                Photos
-              </label>
-              <div class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
-                <div class="text-center">
-                  <svg
-                    class="mx-auto h-12 w-12 text-gray-300"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
+              <div>
+                <FormItem>
+                  {getFieldDecorator("agentCompany", {
+                    initialValue: "",
+                    rules: [{ required: false }],
+                  })(<TextInput label="Agent Company" />)}
+                </FormItem>
+              </div>
+              <div className="col-span-full mt-4 bg-slate-100 p-5">
+                <label
+                  htmlFor="photo"
+                  className="block text-sm font-bold leading-6 text-gray-900"
+                >
+                  Agent Photo
+                </label>
+                <div className="mt-2 flex items-center gap-x-3">
+                  <UserCircleIcon
+                    className="h-12 w-12 text-gray-300"
                     aria-hidden="true"
+                  />
+                  <button
+                    type="button"
+                    className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
                   >
-                    <path
-                      fill-rule="evenodd"
-                      d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z"
-                      clip-rule="evenodd"
-                    />
-                  </svg>
-                  <div class="mt-4 flex text-sm leading-6 text-gray-600">
-                    <label
-                      for="file-upload"
-                      class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
-                    >
-                      <span className="self-center">
-                        {galleryLoading ? "Loading..." : "Add Images"}
-                      </span>
-                      <input
-                        id="file-upload"
-                        name="file-upload"
-                        multiple
-                        max={5}
-                        type="file"
-                        class="sr-only"
-                        ref={inputGallery}
-                        accept="image/png, image/jpg, image/jpeg"
-                        onChange={handleGalleryFile}
-                      />
-                    </label>
-                  </div>
-                  <p class="text-xs leading-5 text-gray-600">
-                    PNG, JPG up to 3MB
-                  </p>
+                    Add Photo
+                  </button>
                 </div>
               </div>
-            </div>
-            <div className="flex overflow-x-auto gap-2 flex-row whitespace-nowrap">
-              {gallaryImages.map((image, index) => (
-                <Paper
-                  key={index}
-                  sx={{
-                    backgroundImage: `url(${image})`,
-                    backgroundRepeat: "no-repeat",
-                    backgroundSize: "cover",
-                    height: 150,
-                    width: 150,
-                    flexShrink: 0,
-                    marginTop: "10px",
-                  }}
-                >
-                  <IconButton
-                    sx={{
-                      m: 1,
-                      height: "22px",
-                      width: "22px",
-                      bgcolor: "white",
+              <Stack gap={3}>
+                <FormItem>
+                  {getFieldDecorator("email", {
+                    initialValue: "",
+                    rules: [{ required: true }],
+                  })(<TextInput label="Email" />)}
+                </FormItem>
+                <FormItem>
+                  {getFieldDecorator("phone", {
+                    initialValue: user && user.user_mobile,
+                    rules: [
+                      {
+                        required: true,
+                        message: "Please enter a valid phone number",
+                      },
+                      { validator: validatePhone },
+                    ],
+                  })(
+                    <PhoneInput
+                      label="Mobile Number"
+                      prependIcon={false}
+                      maxLength={10}
+                    />
+                  )}
+                </FormItem>
+                <div className="mt-2">
+                  <label class="block mb-2 font-medium leading-6 text-gray-700">
+                    Address
+                  </label>
+                  <GooglePlacesAutocomplete
+                    selectProps={{
+                      placeholder: "search your place",
+                      value: addressValue,
+                      onChange: getAddressValue,
                     }}
-                    onClick={() => handleImageDelete(index)}
-                  >
-                    <CloseOutlined />
-                  </IconButton>
-                </Paper>
-              ))}
-            </div>
-          </Stack>
+                    apiKey={process.env.REACT_APP_GOOGLE_MAP_KEY}
+                  />
+                </div>
+                <FormItem>
+                  {getFieldDecorator("state", {
+                    initialValue: "",
+                  })(<Select options={US_STATE} fullWidth label="State" />)}
+                </FormItem>
+                RE_LISTING_TYPE
+                <Stack
+                  gap={2}
+                  sx={{ flexDirection: "row", alignItems: "center" }}
+                >
+                  <FormItem>
+                    {getFieldDecorator("country", {
+                      initialValue: "",
+                      rules: [{ required: true }],
+                    })(<Select fullWidth label="Country" options={COUNTRY} />)}
+                  </FormItem>
 
-          <h2 className="text-xl font-bold pt-6 pb-1">Contact Details</h2>
-          <Stack gap={3}>
-            <FormItem>
-              {getFieldDecorator("email", {
-                initialValue: "",
-                rules: [{ required: true }],
-              })(<TextInput label="Email" />)}
-            </FormItem>
-            <FormItem>
-              {getFieldDecorator("phone", {
-                initialValue: user && user.user_mobile,
-                rules: [
-                  {
-                    required: true,
-                    message: "Please enter a valid phone number",
-                  },
-                  { validator: validatePhone },
-                ],
-              })(
-                <PhoneInput
-                  label="Mobile Number"
-                  prependIcon={false}
-                  maxLength={10}
-                />
-              )}
-            </FormItem>
-            <div className="mt-2">
-              <label class="block mb-2 font-medium leading-6 text-gray-700">
-                Address
-              </label>
-              <GooglePlacesAutocomplete
-                selectProps={{
-                  placeholder: "search your place",
-                  value: addressValue,
-                  onChange: getAddressValue,
-                }}
-                apiKey={process.env.REACT_APP_GOOGLE_MAP_KEY}
-              />
-            </div>
-
-            <FormItem>
-              {getFieldDecorator("state", {
-                initialValue: "",
-              })(<Select options={US_STATE} fullWidth label="State" />)}
-            </FormItem>
-
-            <Stack gap={2} sx={{ flexDirection: "row", alignItems: "center" }}>
-              <FormItem>
-                {getFieldDecorator("country", {
-                  initialValue: "",
-                  rules: [{ required: true }],
-                })(<Select fullWidth label="Country" options={COUNTRY} />)}
-              </FormItem>
-
-              <FormItem>
-                {getFieldDecorator("zipcode", {
-                  initialValue: "",
-                })(<TextInput label="Zipcode" type="number" />)}
-              </FormItem>
+                  <FormItem>
+                    {getFieldDecorator("zipcode", {
+                      initialValue: "",
+                    })(<TextInput label="Zipcode" type="number" />)}
+                  </FormItem>
+                </Stack>
+              </Stack>
             </Stack>
-          </Stack>
-        </Stack>
-
-        <button
-          onClick={checkout}
-          type="button"
-          className="rounded-full bg-black px-8 py-4 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 mt-8"
-        >
-          {isLoading ? "Loading..." : "Continue to Payment"}
-        </button>
+          </div>
+        </div>
+        <div className="col-span-12">
+          <button
+            onClick={checkout}
+            type="button"
+            className="rounded-full bg-black px-8 py-4 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 mt-8"
+          >
+            {isLoading ? "Loading..." : "Continue to Payment"}
+          </button>
+        </div>
       </div>
     </div>
   );
