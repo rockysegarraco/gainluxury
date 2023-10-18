0e1bd42330efcd08b0fc43db3c6f8070b83b0191
SagarChavada <sgchavada2013@gmail.com>
2023-10-18 18:44:38 +0530

    Fix issue

--- src/components/MyListings.js
+++ src/components/MyListings.js
@@ -6,7 +6,7 @@ export default function MyListings({ item, index }) {
         key={index}
         Link={`/${item.category.value}/${item.slug}`}
         Title={item.title}
-        ImageUrl={item.gallery[0]}
+        ImageUrl={item.gallery}
         Alt={item.title}
         Price={
           item.pricingType.value === "Fixed"
