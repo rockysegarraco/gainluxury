50505340bf220002ac7931bef5e8e708043e216f
Rocky Segarra <rockysegarra@icloud.com>
2023-10-18 15:54:49 -0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -94,7 +94,7 @@ export default function Example() {
       <div className="mx-auto max-w-[90%] px-6 pb-8 pt-16 sm:pt-24 lg:px-0 lg:pt-32">
         <div className="xl:grid xl:grid-cols-3 xl:gap-8">
           <div className="space-y-8">
-            <img className="h-7" src="logo-dark.svg" alt="Gain Luxury" />
+            <img className="h-7" src="/logo-dark.svg" alt="Gain Luxury" />
             <p className="text-sm leading-6 text-gray-300">
               Making the world a better place through constructing elegant
               hierarchies.
