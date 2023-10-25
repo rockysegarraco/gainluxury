920b45fe69048637b48cee84124d7959c1db4be7
Rocky Segarra <rockysegarra@icloud.com>
2023-10-25 14:10:26 -0400

    updated ui

--- src/components/Dialog/FullScreenDialog.js
+++ src/components/Dialog/FullScreenDialog.js
@@ -2,11 +2,10 @@ import React, { useState } from "react";
 import Dialog from "@mui/material/Dialog";
 import Slide from "@mui/material/Slide";
 import Stack from "@mui/material/Stack";
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
 import ImageGalleryModal from "./ImageGalleryModal";
 import { XMarkIcon } from "@heroicons/react/20/solid";
-
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
@@ -15,19 +14,23 @@ const Transition = React.forwardRef(function Transition(props, ref) {
 function srcset(image, width, height, rows = 1, cols = 1) {
   return {
     src: `${image}`,
-    srcSet: `${image}`
+    srcSet: `${image}`,
   };
 }
 
-export default function FullScreenDialog({ open, setOpen, images, selectedItem = 0 }) {
-
+export default function FullScreenDialog({
+  open,
+  setOpen,
+  images,
+  selectedItem = 0,
+}) {
   const [isDialogOpen, setDialogOpen] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const handleImageClick = (index) => {
     setCurrentIndex(index);
     setDialogOpen(true);
-  }
+  };
 
   return (
     <>
@@ -38,11 +41,10 @@ export default function FullScreenDialog({ open, setOpen, images, selectedItem =
           onClose={setOpen}
           TransitionComponent={Transition}
         >
-        
-          <Stack sx={{ alignItems: 'center' }}>
+          <Stack sx={{ alignItems: "center" }}>
             <ImageList
               sx={{
-                transform: 'translateZ(0)',
+                transform: "translateZ(0)",
               }}
               gap={1}
             >
@@ -50,9 +52,14 @@ export default function FullScreenDialog({ open, setOpen, images, selectedItem =
                 const cols = 1;
                 const rows = 2;
                 return (
-                  <ImageListItem autoFocus={selectedItem === i} key={i} cols={cols} rows={rows}>
+                  <ImageListItem
+                    autoFocus={selectedItem === i}
+                    key={i}
+                    cols={cols}
+                    rows={rows}
+                  >
                     <img
-                      className="w-full h-[800px]"
+                      className="aspect-[16/9] w-full bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
                       src={item}
                       onClick={() => handleImageClick(i)}
                       alt={i}
@@ -64,13 +71,18 @@ export default function FullScreenDialog({ open, setOpen, images, selectedItem =
             </ImageList>
           </Stack>
           <button
-              onClick={setOpen}
-              type="button"
-              className="absolute top-4 right-4 inline-flex items-center bg-[#F2F2F2] rounded-md p-4 text-sm text-gray-900 hover:bg-gray-200"
-            >
-              <XMarkIcon className="h-5 w-5 text-gray-900" aria-hidden="true" />
-            </button>
-          <ImageGalleryModal open={isDialogOpen} setOpen={() => setDialogOpen(!isDialogOpen)} images={images} currentIndex={currentIndex} />
+            onClick={setOpen}
+            type="button"
+            className="absolute top-4 right-4 inline-flex items-center bg-[#F2F2F2] rounded-md p-4 text-sm text-gray-900 hover:bg-gray-200"
+          >
+            <XMarkIcon className="h-5 w-5 text-gray-900" aria-hidden="true" />
+          </button>
+          <ImageGalleryModal
+            open={isDialogOpen}
+            setOpen={() => setDialogOpen(!isDialogOpen)}
+            images={images}
+            currentIndex={currentIndex}
+          />
         </Dialog>
       </div>
     </>
