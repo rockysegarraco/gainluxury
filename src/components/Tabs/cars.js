4ac5c38c877f459b81aeefdce7ca9d40219bd233
Zangetsu <sgchavada2013@gmail.com>
2023-10-31 12:27:49 +0530

    fix import issue

--- src/components/Tabs/cars.js
+++ src/components/Tabs/cars.js
@@ -3,25 +3,25 @@ import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 //
-import Container from "../components/container.js";
-import CardCar from "../components/cardCar.js";
-import Footer from "../components/Footer";
+import Container from "../container.js";
+import CardCar from "../cardCar.js";
+import Footer from "../Footer";
 
-import SelectCountries from "../components/Selects/SelectCountries";
-import SelectStates from "../components/Selects/SelectStates";
-import SelectMakes from "../components/Selects/SelectMakes";
-import SelectPrice from "../components/Selects/SelectPrice";
-import SelectYears from "../components/Selects/YearSelect";
-import SelectModel from "../components/Selects/SelectModel.js";
-import Filters from "../components/Filters";
-import Searchbar from "../components/Dialog/Searchbar.js";
+import SelectCountries from "../Selects/SelectCountries";
+import SelectStates from "../Selects/SelectStates";
+import SelectMakes from "../Selects/SelectMakes";
+import SelectPrice from "../Selects/SelectPrice";
+import SelectYears from "../Selects/YearSelect";
+import SelectModel from "../Selects/SelectModel.js";
+import Filters from "../Filters";
+import Searchbar from "../Dialog/Searchbar.js";
 //
-import db from "../firebase";
+import db from "../../firebase";
 import Stack from "@mui/material/Stack";
-import { BRAND, COUNTRY } from "../utils/constants.js";
+import { BRAND, COUNTRY } from "../../utils/constants.js";
 
 // redux
-import { setBrand, setModel, setModelData } from "../store/brandSerchSlice.js";
+import { setBrand, setModel, setModelData } from "../../store/brandSerchSlice.js";
 import { useDispatch, useSelector } from "react-redux";
 
 const Cars = () => {
