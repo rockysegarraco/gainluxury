0f033f80b1856f25bd601b33b1a23e749d19b7ab
Rocky Segarra <rockysegarra@icloud.com>
2023-11-13 22:45:51 -0500

    updated ui

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -24,7 +24,7 @@ export default function Example() {
       <div className="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2">
         <div className="relative px-6 pb-20 pt-24 sm:pt-32 lg:static lg:px-8 lg:py-48">
           <div className="mx-auto max-w-xl lg:mx-0 lg:max-w-lg">
-            <div className="absolute inset-y-0 left-0 -z-10 w-full overflow-hidden bg-gray-100 ring-1 ring-gray-900/10 lg:w-1/2">
+            <div className="absolute inset-y-0 left-0 -z-10 w-full overflow-hidden bg-gray-100  lg:w-1/2">
               <svg
                 className="absolute inset-0 h-full w-full stroke-gray-200 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
                 aria-hidden="true"
