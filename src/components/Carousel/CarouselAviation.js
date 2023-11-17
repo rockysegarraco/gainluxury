a8af1bb89cf3b4c722b9c2f1cf2960296d857de1
Rocky Segarra <rockysegarra@icloud.com>
2023-11-16 20:49:54 -0500

    updated aviation carousel

---
+++ src/components/Carousel/CarouselAviation.js
@@ -0,0 +1,141 @@
+import React, { useState, useEffect } from "react";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+
+import "./Carousel.css";
+
+const content = [
+  <div className="relative h-full w-full">
+    <img
+      src="https://images.unsplash.com/photo-1474302770737-173ee21bab63?q=80&w=3608&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      alt="image 3"
+      className="h-full w-full object-cover"
+    />
+    <div className="absolute inset-0 bottom-0 grid h-full w-full place-items-center bg-black/50">
+      <div className="absolute left-4 lg:left-16 bottom-0">
+        <div className="text-4xl lg:text-6xl text-white fancy mb-8 lg:leading-[4.5rem]">
+          Aviation
+        </div>
+      </div>
+    </div>
+  </div>,
+];
+
+const Carousel = () => {
+  const [counter, setCounter] = useState(1);
+  const [pause, setPause] = useState(false);
+
+  const handleNext = () => {
+    if (counter !== content.length) {
+      setCounter(counter + 1);
+    } else {
+      setCounter(1);
+    }
+  };
+
+  const handlePre = () => {
+    if (counter !== 1) {
+      setCounter(counter - 1);
+    } else {
+      setCounter(content.length);
+    }
+  };
+
+  const handlePage = (page) => {
+    setCounter(page);
+  };
+
+  const handleMouse = () => {
+    setPause(!pause);
+  };
+
+  useEffect(() => {
+    let interval = setInterval(() => {
+      if (!pause) {
+        handleNext();
+      } else {
+        clearInterval(interval);
+      }
+    }, 9000);
+    return () => clearInterval(interval);
+  });
+
+  return (
+    <div className="App">
+      <div
+        className="slide"
+        onMouseEnter={handleMouse}
+        onMouseLeave={handleMouse}
+      >
+        {content.map((item, index) => (
+          <div
+            className={counter - 1 === index ? "show" : "not-show"}
+            key={index}
+          >
+            {item}
+          </div>
+        ))}
+
+        <IconButton
+          variant="text"
+          color="white"
+          size="lg"
+          onClick={handlePre}
+          className="!absolute top-2/4 left-4 -translate-y-2/4 "
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={2}
+            stroke="currentColor"
+            className="h-6 w-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+            />
+          </svg>
+        </IconButton>
+
+        <IconButton
+          variant="text"
+          color="white"
+          size="lg"
+          onClick={handleNext}
+          className="!absolute top-2/4 !right-4 -translate-y-2/4"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={2}
+            stroke="currentColor"
+            className="h-6 w-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+            />
+          </svg>
+        </IconButton>
+      </div>
+
+      <div className="absolute bottom-24 lg:bottom-28 left-2/4 z-50 flex -translate-x-2/4 gap-2 hidden">
+        {new Array(content.length).fill("").map((_, i) => (
+          <span
+            key={i}
+            className={`block h-1 cursor-pointer rounded-2xl transition-all content-[''] ${
+              counter - 1 === i ? "w-8 bg-white" : "w-4 bg-white/50"
+            }`}
+            onClick={() => handlePage(i + 1)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
--- src/routes/aviator.jsx
+++ src/routes/aviator.jsx
@@ -3,7 +3,7 @@ import { collection, getDocs, query, where } from "firebase/firestore";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
-import CarouselCars from "../components/Carousel/CarouselCars";
+import CarouselAviation from "../components/Carousel/CarouselAviation";
 //
 import Container from "../components/container.js";
 
@@ -235,7 +235,7 @@ const Aviation = () => {
 
   return (
     <>
-      <CarouselCars />
+      <CarouselAviation />
       <div className="flex flex-col">
         <div className="border-b py-3 lg:py-3 max-w-[100vw]">
           <div className="flex flex-row mx-auto px-4 lg:px-20 overflow-scroll">
@@ -300,7 +300,6 @@ const Aviation = () => {
           </div>
         </div>
         <Container>
-          <h1 className="text-2xl lg:text-4xl fancy pt-4">Aviation for Sale</h1>
           <Stack
             sx={{
               display: "flex",
