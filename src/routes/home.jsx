3e918ab3da3c4be777714cf7ef2984222949899a
Zangetsu <sgchavada2013@gmail.com>
2023-11-28 21:34:30 +0530

    fix issue

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,4 +1,4 @@
-import { collection, getDocs, query, orderBy, limit } from "firebase/firestore";
+import { collection, getDocs, query, orderBy, limit, where } from "firebase/firestore";
 import { useEffect, useState } from "react";
 
 import db from "../firebase";
@@ -19,35 +19,35 @@ function Home() {
   const getPost = async () => {
     const data = [];
     const carCollection = collection(db, "cars");
-    const cq = query(carCollection, orderBy("postDate", "asc"), limit(2));
+    const cq = query(carCollection,  where('postStatus', "==", "Live"), orderBy("postDate", "asc"), limit(2));
     const csnap = await getDocs(cq);
     csnap.forEach((doc) => {
       // doc.data() is never undefined for query doc snapshots
       data.push(doc.data());
     });
     const propertyCollection = collection(db, "properties");
-    const pq = query(propertyCollection, orderBy("postDate", "asc"), limit(2));
+    const pq = query(propertyCollection,  where('postStatus', "==", "Live"), orderBy("postDate", "asc"), limit(2));
     const psnap = await getDocs(pq);
     psnap.forEach((doc) => {
       // doc.data() is never undefined for query doc snapshots
       data.push(doc.data());
     });
     const marineCollection = collection(db, "marine");
-    const mq = query(marineCollection, orderBy("postDate", "asc"), limit(2));
+    const mq = query(marineCollection, where('postStatus', "==", "Live"), orderBy("postDate", "asc"), limit(2));
     const msnap = await getDocs(mq);
     msnap.forEach((doc) => {
       // doc.data() is never undefined for query doc snapshots
       data.push(doc.data());
     });
     const aviationCollection = collection(db, "aviation");
-    const aq = query(aviationCollection, orderBy("postDate", "asc"), limit(2));
+    const aq = query(aviationCollection,  where('postStatus', "==", "Live"), orderBy("postDate", "asc"), limit(2));
     const asnap = await getDocs(aq);
     asnap.forEach((doc) => {
       // doc.data() is never undefined for query doc snapshots
       data.push(doc.data());
     });
     const artCollection = collection(db, "arts");
-    const artq = query(artCollection, orderBy("postDate", "asc"), limit(2));
+    const artq = query(artCollection,  where('postStatus', "==", "Live"), orderBy("postDate", "asc"), limit(2));
     const artsnap = await getDocs(artq);
     artsnap.forEach((doc) => {
       // doc.data() is never undefined for query doc snapshots
